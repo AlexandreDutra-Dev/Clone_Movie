a6d8d0e9428907124806b271e4132a046c7f1017
Alexandre <jalexandredutra@gmail.com>
2023-02-19 16:43:02 -0300

    Ao pesquisar por um filme,esta sendo exibido somente o filme da lista

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { MoviesService } from 'src/app/service/movies.service';
-import { delay } from 'rxjs/internal/operators/delay';
+import { Component, OnInit } from "@angular/core";
+import { MoviesService } from "src/app/service/movies.service";
+import { delay } from "rxjs/internal/operators/delay";
 
 @Component({
-  selector: 'app-movies',
-  templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+  selector: "app-movies",
+  templateUrl: "./movies.component.html",
+  styleUrls: ["./movies.component.scss"],
 })
 export class MoviesComponent implements OnInit {
   topRated: any;
@@ -15,25 +15,28 @@ export class MoviesComponent implements OnInit {
   total_results: any;
   searchRes: any;
   searchStr: string;
+  suggestions: { title: string }[] = [];
+  showSuggestions = false;
+  selectedIndex = -1;
 
   constructor(private movieService: MoviesService) {
     this.responsiveOptions = [
       {
-          breakpoint: '1024px',
-          numVisible: 3,
-          numScroll: 3
+        breakpoint: "1024px",
+        numVisible: 3,
+        numScroll: 3,
       },
       {
-          breakpoint: '768px',
-          numVisible: 2,
-          numScroll: 2
+        breakpoint: "768px",
+        numVisible: 2,
+        numScroll: 2,
       },
       {
-          breakpoint: '560px',
-          numVisible: 1,
-          numScroll: 1
-      }
-  ];
+        breakpoint: "560px",
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
   }
 
   ngOnInit() {
@@ -41,12 +44,17 @@ export class MoviesComponent implements OnInit {
   }
 
   getTopRatedMovies(page: number) {
-    this.movieService.getTopRatedMovies(page).pipe(delay(2000)).subscribe((res: any) => {
-      this.topRated = res.results;
-      this.totalResults = res.total_results;
-      this.loader = false;
-    },
-    error => console.log(error));
+    this.movieService
+      .getTopRatedMovies(page)
+      .pipe(delay(2000))
+      .subscribe(
+        (res: any) => {
+          this.topRated = res.results;
+          this.totalResults = res.total_results;
+          this.loader = false;
+        },
+        (error) => console.log(error)
+      );
   }
 
   changePage(event) {
@@ -54,11 +62,67 @@ export class MoviesComponent implements OnInit {
     this.getTopRatedMovies(event.pageIndex + 1);
   }
 
-  searchMovies() {
-    this.movieService.searchMovies(this.searchStr).subscribe(res => {
-      this.searchRes = res.results;
+  onSearchKeyup(event: any) {
+    if (event.target.value.length >= 3) {
+      this.searchMovies();
+    }
+  }
+
+  searchSuggestions(): void {
+    this.movieService.searchMovies(this.searchStr).subscribe((res) => {
+      this.suggestions = res.results;
+      this.showSuggestions = true;
     });
   }
 
+  selectSuggestion(suggestion: any): void {
+    this.searchStr = suggestion.title;
+    this.showSuggestions = false;
+  }
 
+  onKeyDown(event): void {
+    if (event.key === "ArrowUp") {
+      this.selectedIndex =
+        this.selectedIndex > 0
+          ? this.selectedIndex - 1
+          : this.suggestions.length - 1;
+    } else if (event.key === "ArrowDown") {
+      this.selectedIndex =
+        this.selectedIndex < this.suggestions.length - 1
+          ? this.selectedIndex + 1
+          : 0;
+    } else if (event.key === "Enter") {
+      event.preventDefault();
+      if (
+        this.selectedIndex >= 0 &&
+        this.selectedIndex < this.suggestions.length
+      ) {
+        this.searchStr = this.suggestions[this.selectedIndex].title;
+      }
+      this.showSuggestions = false;
+    }
+  }
+
+  showSuggestion() {
+    this.showSuggestions = true;
+  }
+
+  searchMovies() {
+    this.showSuggestions = false;
+    this.selectedIndex = -1;
+    this.suggestions = [];
+
+    this.movieService.searchMovies(this.searchStr).subscribe((res) => {
+      this.loader = false;
+      this.searchRes = res["results"];
+      this.totalResults = res["total_results"];
+      this.topRated = []; // Limpa a lista de filmes carregados anteriormente
+
+      // se o campo de pesquisa estiver vazio, carrega a lista de filmes novamente
+      if (this.searchStr === "") {
+        this.loader = true;
+        this.getTopRatedMovies(1);
+      }
+    });
+  }
 }
--- src/app/service/movies.service.ts
+++ src/app/service/movies.service.ts
@@ -3,7 +3,7 @@ import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class MoviesService {
   baseUrl: string;
@@ -12,82 +12,121 @@ export class MoviesService {
   region: string;
 
   constructor(private http: HttpClient) {
-    this.baseUrl = 'https://api.themoviedb.org/3/';
-    this.apiKey = 'dd4d819639705d332d531217b4f7c6b6';
-    this.language = 'pt-BR';
-    this.region = 'BR';
+    this.baseUrl = "https://api.themoviedb.org/3/";
+    this.apiKey = "dd4d819639705d332d531217b4f7c6b6";
+    this.language = "pt-BR";
+    this.region = "BR";
   }
 
   getNowPlaying(page: number): Observable<any> {
     // tslint:disable-next-line: max-line-length
-    return this.http.get(`${this.baseUrl}movie/now_playing?api_key=${this.apiKey}&page=${page}&language=${this.language}&region=${this.region}`);
+    return this.http.get(
+      `${this.baseUrl}movie/now_playing?api_key=${this.apiKey}&page=${page}&language=${this.language}&region=${this.region}`
+    );
   }
 
   searchMovies(searchStr: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}search/movie?api_key=${this.apiKey}&query=${searchStr}&language=${this.language}&region=${this.region}`);
+    return this.http.get(
+      `${this.baseUrl}search/movie?api_key=${this.apiKey}&query=${searchStr}&language=${this.language}&region=${this.region}`
+    );
+  }
+
+  searchSuggestions(searchStr: string): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}search/multi?api_key=${this.apiKey}&query=${searchStr}&language=${this.language}&region=${this.region}`
+    );
   }
 
   getPopular(page: number): Observable<any> {
     // tslint:disable-next-line: max-line-length
-    return this.http.get(`${this.baseUrl}movie/popular?api_key=${this.apiKey}&page=${page}&language=${this.language}&region=${this.region}`);
+    return this.http.get(
+      `${this.baseUrl}movie/popular?api_key=${this.apiKey}&page=${page}&language=${this.language}&region=${this.region}`
+    );
   }
 
   getUpComingMovies(page: number): Observable<any> {
     // tslint:disable-next-line: max-line-length
-    return this.http.get(`${this.baseUrl}movie/upcoming?api_key=${this.apiKey}&page=${page}&language=${this.language}&region=${this.region}`);
+    return this.http.get(
+      `${this.baseUrl}movie/upcoming?api_key=${this.apiKey}&page=${page}&language=${this.language}&region=${this.region}`
+    );
   }
 
   getTopRatedMovies(page: number): Observable<any> {
     // tslint:disable-next-line: max-line-length
-    return this.http.get(`${this.baseUrl}movie/top_rated?api_key=${this.apiKey}&page=${page}&language=${this.language}&region=${this.region}`);
+    return this.http.get(
+      `${this.baseUrl}movie/top_rated?api_key=${this.apiKey}&page=${page}&language=${this.language}&region=${this.region}`
+    );
   }
 
   getDiscoverMovies(): Observable<any> {
-    return this.http.get(`${this.baseUrl}discover/movie?api_key=${this.apiKey}`);
+    return this.http.get(
+      `${this.baseUrl}discover/movie?api_key=${this.apiKey}&language=${this.language}&region=${this.region}`
+    );
   }
 
   getGenres(): Observable<any> {
-    return this.http.get(`${this.baseUrl}genre/movie/list?api_key=${this.apiKey}&language=${this.language}`);
+    return this.http.get(
+      `${this.baseUrl}genre/movie/list?api_key=${this.apiKey}&language=${this.language}`
+    );
   }
 
   getMoviesByGenre(id: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}genre/${id}/movies?api_key=${this.apiKey}&language=${this.language}`);
+    return this.http.get(
+      `${this.baseUrl}genre/${id}/movies?api_key=${this.apiKey}&language=${this.language}`
+    );
   }
 
   getMovie(id: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}movie/${id}?api_key=${this.apiKey}&language=${this.language}`);
+    return this.http.get(
+      `${this.baseUrl}movie/${id}?api_key=${this.apiKey}&language=${this.language}`
+    );
   }
 
   getMovieReviews(id: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}movie/${id}/reviews?api_key=${this.apiKey}&language=${this.language}`);
+    return this.http.get(
+      `${this.baseUrl}movie/${id}/reviews?api_key=${this.apiKey}&language=${this.language}`
+    );
   }
 
   getMovieCredits(id: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}movie/${id}/credits?api_key=${this.apiKey}&language=${this.language}`);
+    return this.http.get(
+      `${this.baseUrl}movie/${id}/credits?api_key=${this.apiKey}&language=${this.language}`
+    );
   }
 
   getBackdropsImages(id: string) {
-    return this.http.get(`${this.baseUrl}movie/${id}/images?api_key=${this.apiKey}&language=${this.language}`);
+    return this.http.get(
+      `${this.baseUrl}movie/${id}/images?api_key=${this.apiKey}&language=${this.language}`
+    );
   }
 
   getMovieVideos(id: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}movie/${id}/videos?api_key=${this.apiKey}&language=${this.language}`);
+    return this.http.get(
+      `${this.baseUrl}movie/${id}/videos?api_key=${this.apiKey}&language=${this.language}`
+    );
   }
 
   getRecomendMovies(id: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}movie/${id}/recommendations?api_key=${this.apiKey}&language=${this.language}`);
+    return this.http.get(
+      `${this.baseUrl}movie/${id}/recommendations?api_key=${this.apiKey}&language=${this.language}`
+    );
   }
 
   getPersonDetail(id: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}person/${id}?api_key=${this.apiKey}&language=${this.language}`);
+    return this.http.get(
+      `${this.baseUrl}person/${id}?api_key=${this.apiKey}&language=${this.language}`
+    );
   }
 
   getPersonExternalData(id: string) {
-    return this.http.get(`${this.baseUrl}person/${id}/external_ids?api_key=${this.apiKey}&language=${this.language}`);
+    return this.http.get(
+      `${this.baseUrl}person/${id}/external_ids?api_key=${this.apiKey}&language=${this.language}`
+    );
   }
 
   getPersonCast(id: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}person/${id}/movie_credits?api_key=${this.apiKey}&language=${this.language}`);
+    return this.http.get(
+      `${this.baseUrl}person/${id}/movie_credits?api_key=${this.apiKey}&language=${this.language}`
+    );
   }
-
 }
