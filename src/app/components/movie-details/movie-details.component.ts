67c18ab7bc620ab6b9823834028e669cbba3ff6e
Alexandre <jalexandredutra@gmail.com>
2023-02-19 15:08:25 -0300

    fix: corrige erro ao clicar em um filme a pagina não subia para o topo

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -1,65 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-import { MoviesService } from 'src/app/service/movies.service';
-import { ActivatedRoute , Params} from '@angular/router';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MatDialog } from '@angular/material';
-import { AppMovieDialogComponent } from '../movie-details/app-movie-dialog/app-movie-dialog.component';
+import { Component, OnInit } from "@angular/core";
+import { MoviesService } from "src/app/service/movies.service";
+import { ActivatedRoute, Params, Router, NavigationEnd } from "@angular/router";
+import { DomSanitizer } from "@angular/platform-browser";
+import { MatDialog } from "@angular/material";
+import { AppMovieDialogComponent } from "../movie-details/app-movie-dialog/app-movie-dialog.component";
 
 @Component({
-  selector: 'app-movie-details',
-  templateUrl: './movie-details.component.html',
-  styleUrls: ['./movie-details.component.scss'],
+  selector: "app-movie-details",
+  templateUrl: "./movie-details.component.html",
+  styleUrls: ["./movie-details.component.scss"],
 })
 export class MovieDetailsComponent implements OnInit {
   public id: number;
   public video: boolean;
   movie: any;
-  baseUrl = 'https://www.youtube.com/embed/';
-  autoplay = '?rel=0;&autoplay=1&mute=0';
+  baseUrl = "https://www.youtube.com/embed/";
+  autoplay = "?rel=0;&autoplay=1&mute=0";
   relatedvideo: any;
   casts: any = [];
   backdrops: any = [];
   recomendMovies: any = [];
   responsiveOptions;
 
-
   constructor(
     private movieService: MoviesService,
     private router: ActivatedRoute,
     private sanitizer: DomSanitizer,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private route: Router
   ) {
     this.responsiveOptions = [
       {
-          breakpoint: '1024px',
-          numVisible: 3,
-          numScroll: 3
+        breakpoint: "1024px",
+        numVisible: 3,
+        numScroll: 3,
       },
       {
-          breakpoint: '768px',
-          numVisible: 2,
-          numScroll: 2
+        breakpoint: "768px",
+        numVisible: 2,
+        numScroll: 2,
       },
       {
-          breakpoint: '560px',
-          numVisible: 1,
-          numScroll: 1
-      }
-  ];
+        breakpoint: "560px",
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
   }
 
   ngOnInit() {
     this.router.params.subscribe((params: Params) => {
-      this.id = params['id'];
+      this.id = params["id"];
       this.getSingleMoviesVideos(this.id);
       this.getSingleMoviesDetails(this.id);
       this.getCast(this.id);
       this.getBackropsImages(this.id);
       this.getRecomendMovie(this.id);
     });
+
+    this.route.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
   }
 
-  getSingleMoviesDetails(id){
+  getSingleMoviesDetails(id) {
     this.movieService.getMovie(id).subscribe((res: any) => {
       this.movie = res;
     });
@@ -75,11 +81,13 @@ export class MovieDetailsComponent implements OnInit {
   }
 
   openDialogMovie(video): void {
-    this.video['url'] = this.sanitizer.bypassSecurityTrustResourceUrl(this.baseUrl + video.key + this.autoplay);
+    this.video["url"] = this.sanitizer.bypassSecurityTrustResourceUrl(
+      this.baseUrl + video.key + this.autoplay
+    );
     this.dialog.open(AppMovieDialogComponent, {
-      height: '600px',
-      width: '900px',
-      data: { video: this.video}
+      height: "600px",
+      width: "900px",
+      data: { video: this.video },
     });
   }
 
@@ -100,6 +108,4 @@ export class MovieDetailsComponent implements OnInit {
       this.recomendMovies = res.results;
     });
   }
-
 }
-
